e65468447027daf85131d6cb5f6d3b41ba9d5a46
Christopher Chalcraft <chris.chalcraft@gmail.com>
2020-11-30 00:04:58 -0700

    Added the new changes to main and generating figs

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os #operating system
 import pandas as pd #pandas for dataframe management
 import matplotlib.pyplot as plt #matplotlib for plotting
 import matplotlib.dates as mdates # alias for date formatting
+import numpy as np # for generating synthetic data
 
 # Data
 dataPath = './' # set data path
@@ -11,14 +12,37 @@ df['date'] = pd.to_datetime(df['date']) # convert column to datetime
 df.set_index('date', inplace=True) # set date as the index
 df_avg = df.resample('D').mean() # resample by DAY and take the mean and create a new dataframe called df_avg
 
-#plot data
-# You would need to experiment around with some of the plot settings to get the desired result
-fig, ax = plt.subplots(figsize=(15,7))
-df_avg.plot(ax=ax, kind='bar')
-plt.show()
 
+############################ NEW #############################
 
-# #set ticks every week
-# ax.xaxis.set_major_locator(mdates.WeekdayLocator())
-# #set major ticks format
-# ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %d'))
+# Moving on .. synthetic data
+# This extracts the data as an array, creates a sythetic interval, samples the data based on provided data, and creates a new time series
+data = df['demand'].values
+interval = pd.date_range('2018-11-01', '2020-10-30', freq='15min')
+synthetic_data = np.random.choice(data, len(interval))
+ts = pd.DataFrame({'demand': synthetic_data}, index=interval)
+ts.index.name = 'date'
+
+# resampling analysis 
+ts_daily = ts.resample('D').mean()
+days = ts_daily.index.strftime("%Y-%m-%d")
+ts_monthly = ts.resample('M').mean()
+months = ts_monthly.index.strftime("%Y-%m")
+
+# Here we can just loop over what months we want to generate plots for
+for month in months[2:5]:
+    fig, ax = plt.subplots(figsize=(15,7))
+    ts_daily[month].plot(ax=ax, kind='bar')
+
+    # display
+    plt.title("Monthly demand for {}".format(month))
+    plt.xlabel("day of month")
+    plt.ylabel('demand')
+
+    # #set ticks every week
+    ax.xaxis.set_major_locator(mdates.WeekdayLocator())
+    #set major ticks format
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%d'))
+
+    # plt.show()
+    plt.savefig('figures/{}'.format(month))
